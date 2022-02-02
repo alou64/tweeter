acd00d8d03fd5a798fd166f1af8a0e25afb0f263
alou <01008@protonmail.com>
2022-02-02 12:33:35 -0800

    implemented ajax get request to fetch tweets from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -43,46 +43,30 @@ const createTweetElement = data => {
 };
 
 
-
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-
-
 $(document).ready(function() {
-  renderTweets(data);
 
   // event listener for submit button
   $('form').submit(function(event) {
     event.preventDefault();    // prevent button from refreshing page
     const tweet = $(this).serialize();
+    // send tweet to server
     $.post('/tweets/', tweet, function(data, status) {
       if (status === 'success') {
         console.log('Tweet sent to server');
       }
     });
   });
+
+  // fetch tweets server
+  const loadTweets = function() {
+    $.get('http://localhost:8080/tweets', function(data, status) {
+      if (status === 'success') {
+        console.log('Fetched tweets from server');
+      }
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
 
 });
