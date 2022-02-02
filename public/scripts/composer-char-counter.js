56fb970d86d08373539093c1b3009ec0ae185ed5
alou <01008@protonmail.com>
2022-02-01 18:18:22 -0800

    added jquery for character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  console.log('ready!');
+
+  $('#tweet-text').on('input', function(){
+    let remainingChars = 140 - $(this).val().length;
+    if (remainingChars < 1) {
+      $('#counter').css('color', 'red');
+    } else {
+      $('#counter').css('color', 'black');
+    }
+    $('#counter').text(remainingChars);
+  });
+
+});
